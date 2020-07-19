989987a74f0fbd124bc9ff44e9b163c32260cb24
pockerman <a.giavaras@gmail.com>
2020-07-19 13:30:48 +0100

    Update dc motor python code

--- python/dc_motor.py
+++ python/dc_motor.py
@@ -1,6 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
+def turn_clockwise(in1_pin, in2_pin):
+	GPIO.output(in1_pin, True)
+	GPIO.output(in2_pin, False)
+
+def turn_anticlockwise(in1_pin, in2_pin):
+	GPIO.output(in1_pin, False)
+	GPIO.output(in2_pin, True)
+
+
 if __name__ == '__main__':
 
 	ENABLE_PIN = 18
@@ -12,4 +21,23 @@ if __name__ == '__main__':
 	GPIO.setup(ENABLE_PIN, GPIO.OUT)
 	GPIO.setup(IN1_PIN, GPIO.OUT)
 	GPIO.setup(IN2_PIN, GPIO.OUT)
+
+	# set up the speed to the spin
+	pwm = GPIO.PWM(ENABLE_PIN, 100)
+	pwm.start(0)
+
+	while True:
+
+		try:
+			turn_clockwise(in1_pin=IN1_PIN, in2_pin=IN2_PIN)
+			time.sleep(1)
+			turn_anticlockwise(in1_pin=IN1_PIN, in2_pin=IN2_PIN)
+			time.sleep(1)
+		except  KeyboardInterrupt:
+			break
+
+	# cleanup the pins
+	GPIO.cleanup()
+
+		
 
